1ff98490a9a09c66c72c17d83d126979a60801d0
natanael <nbrizuela@fi.uba.ar>
2024-07-01 18:39:37 -0300

    se agrego la funcionalidad de agregar clientes

--- frontend/hace-tu-pedido/script.js
+++ frontend/hace-tu-pedido/script.js
@@ -1,9 +1,35 @@
+function handle_response(data) {
+  if (data != null) {
+    window.location.href = `../index.html`;
+  } else {
+    console.log(data);
+    alert("eRROR");
+  }
+}
 
+function crearCliente(datos) {
+  datos.preventDefault();
 
-function pasarPagina(){
-    const nombreApellido = document.getElementById('nombre-apellido').value;
-    const direccion = document.getElementById('direccion').value;
-    const telefono = document.getElementById('telefono').value;
-    const mail = document.getElementById('mail').value;
-    location.href = 'paso2/paso2.html'; 
+  datosForm = new FormData(datos.target);
+
+  const nombreApellido = datosForm.get("nombre-apellido");
+  const direccion = datosForm.get("direccion");
+  const telefono = datosForm.get("telefono");
+  const mail = datosForm.get("mail");
+
+  fetch("http://localhost:5000/clientes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre_apellido: nombreApellido,
+      direccion: direccion,
+      telefono: telefono,
+      mail: mail,
+    }),
+  })
+    .then((res) => res.json())
+    .then(handle_response)
+    .then((error) => console.loog("Error: ", error));
 }
