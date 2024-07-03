9adfd14e104a6c8e5455f400ccd79e0186d4f5ff
Ana <anaielizondo448@gmail.com>
2024-07-03 19:11:29 -0300

    Se arreglo el envio de los datos de una pagina a otra

--- frontend/hace-tu-pedido/script.js
+++ frontend/hace-tu-pedido/script.js
@@ -1,37 +1,46 @@
+let mail;
+
+
 function handle_response(data) {
-    if (data != null) {
-      window.location.href = 'paso2/paso2.html';
-    } else {
-      console.log(data);
-      alert("ERROR");
-    }
+  if (data != null) {
+    window.location.href = `paso2?mail=${mail}`;
+    alert("todo b   ien :D")
+  } else {
+    console.log(data);
+    alert("ERROR");
   }
-  
-  function crearCliente(datos) {
-    datos.preventDefault();
-  
-    datosForm = new FormData(datos.target);
-  
-    const nombreApellido = datosForm.get("nombre-apellido");
-    const direccion = datosForm.get("direccion");
-    const telefono = datosForm.get("telefono");
-    const mail = datosForm.get("mail");
-  
-    fetch("http://localhost:5000/clientes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        nombre_apellido: nombreApellido,
-        direccion: direccion,
-        telefono: telefono,
-        mail: mail,
-      }),
-    })
-      .then((res) => res.json())
-      .then(handle_response)
-      .then((error) => console.log("Error: ", error));
-  }
+}
+
+
+
+function crearCliente(datos) {
+  datos.preventDefault();
   
 
+  datosForm = new FormData(datos.target);
+
+  const nombreApellido = datosForm.get("nombre-apellido");
+  const direccion = datosForm.get("direccion");
+  const telefono = datosForm.get("telefono");
+   mail = datosForm.get("mail");
+
+  fetch("http://localhost:5000/clientes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre_apellido: nombreApellido,
+      direccion: direccion,
+      telefono: telefono,
+      mail: mail,
+    }),
+  })
+    .then((res) => res.json())
+    .then(handle_response)
+    .then((error) => console.log("Error: ", error));
+}
+
+function pasarPagina(event){
+  crearCliente(event)
+}
