573fed0962515d87a886b0efad6f0f18493d0a0e
valentinarossi1 <valenrossi122@gmail.com>
2024-07-05 01:57:37 -0300

    se agrego el error handler para el error 404

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask_cors import CORS
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from models import db, Clientes, Panes, Pedidos
 
 
@@ -114,7 +114,11 @@ def nuevo_cliente():
     except Exception as e:
         return jsonify(f"no se  pudo :{e})"), 400
 
+@app.errorhandler(404)
+def pagina_no_encontrada():
 
+    return render_template('../frontend/404.html')
+    
 if __name__ == '__main__':
     print('Starting server...')
     db.init_app(app)
